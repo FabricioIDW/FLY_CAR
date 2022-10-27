9ec00904cdc65ce9c29b2f11ef0721aeae9eda2f
FabricioIDW <108549537+FabricioIDW@users.noreply.github.com>
2022-10-26 22:56:32 -0300

    Update postcss.config.js
    arreglando bug

--- postcss.config.js
+++ postcss.config.js
@@ -5,10 +5,3 @@ module.exports = {
         autoprefixer: {},
     },
 };
-=======
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 59dca736b72a597783d275273ea311dd68cb37f7
